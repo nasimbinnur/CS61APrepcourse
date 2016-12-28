e4d2f7de13fc89bbd1f9bdab25b8dd97cff21907
Nasim Binnur <nasimbinnur@gmail.com>
2016-12-27 22:21:15 -0800

    Week 1

---
+++ ch5/BiggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts "What is your favorite number?"
+fnumber = gets
+bbnumber = fnumber.to_i + 1
+puts bbnumber.to_s + " is a bigger and better number!"
---
+++ ch5/FullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts "What is your First Name?"
+fname = gets.chomp 
+puts "What is your Middle Name?"
+mname = gets.chomp 
+puts = "What is your Last Name?"
+lname = gets.chomp
+puts "Hello " + fname + " "+ mname + " " + lname + ". Nice to meet you!"
